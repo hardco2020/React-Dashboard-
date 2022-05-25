50635bbab25c170e8cfb25f008ed43b970eceb37
Kai <hardco2020@gmail.com>
2022-05-25 12:15:42 +0800

    add: initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+const style = {
+  wrapper: "bg-fuchsia-400",
+  wrapperTitle: "text-white",
+};
+
+function App() {
+  return (
+    <div className={style.wrapper}>
+      lama dev
+      <h1 className={style.wrapperTitle}>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
